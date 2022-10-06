356de7f6b93cca8ec61b0cbf94c6a706c8c74e1f
PomortsevaAnastasia <massage.anastasia@mail.ru>
2022-10-06 13:22:24 +0300

    сравнение чисел и нахождение мах

---
+++ Example6/Program.cs
@@ -0,0 +1,16 @@
+﻿int a=1;
+int b=2;
+int c=4;
+int d=6;
+int e=8;
+
+int max=a;
+
+if (a>max) max=1;
+if (b>max) max=2;
+if (c>max) max=4;
+if (d>max) max=6;
+if (e>max) max=8;
+
+Console.Write ("max= ");
+Console.WriteLine(max);
